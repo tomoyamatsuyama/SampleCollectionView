87b0f53341adf8ee14b9f35a7c854c782827f6a7
松山友也 <>
2017-11-14 19:00:14 +0900

    first commit

---
+++ SampleCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleCollectionView
+//
+//  Created by 松山友也 on 2017/11/14.
+//  Copyright © 2017年 Tomoya Matsuyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
